aae9cf6343d5fb799776a92c59b921ebe1047d97
btduarte <80214791+btduarte@users.noreply.github.com>
2022-06-03 01:40:01 -0300

    Add files via upload

---
+++ EntregaFinal_todo-List/app.js
@@ -0,0 +1,110 @@
+'use strict';
+
+let banco = [];
+
+const getBanco = () => JSON.parse(localStorage.getItem ('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem ('todoList', JSON.stringify(banco));
+
+const criarItem = (tarefa, status, indice) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+    `;
+    document.getElementById('todoList').appendChild(item);
+}
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    const banco = getBanco(); 
+    banco.forEach ( (item, indice) => criarItem (item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        const banco = getBanco();
+        banco.push ({'tarefa': texto, 'status': ''});
+        setBanco(banco);
+        atualizarTela();
+        evento.target.value = '';
+    }
+}
+
+const removerItem = (indice) => {
+    const banco = getBanco();
+    banco.splice (indice, 1);
+    setBanco(banco);
+    atualizarTela();
+}
+
+const atualizarItem = (indice) => {
+    const banco = getBanco();
+    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    setBanco(banco);
+    atualizarTela();
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    console.log (elemento.type);
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem (indice);
+    }else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+        atualizarItem (indice);
+    }
+}
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
+
+function handleCredentialResponse(response) {
+  const data = jwt_decode(response.credential)
+
+  fullName.textContent = data.name
+  sub.textContent = data.sub
+  given_name.textContent = data.given_name
+  family_name.textContent = data.family_name
+  email.textContent = data.email
+  verifiedEmail.textContent = data.email_verified
+  picture.setAttribute("src", data.picture)
+}
+
+window.onload = function () {
+  const clientID = window.prompt("Bem-vindo ao projeto Invictos!", "")
+
+  google.accounts.id.initialize({
+    client_id: clientID,
+    callback: handleCredentialResponse
+  });
+
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"), {
+    theme: "filled_black",
+    size: "large",
+    type: "standard",
+    shape: "pill",
+    locale: "pt-BR",
+    logo_alignment: "left",
+  } // customization attributes
+  );
+
+  google.accounts.id.prompt(); // also display the One Tap dialog
+}
+
+
+
+atualizarTela();
